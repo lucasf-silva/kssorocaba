6803bd4ee1a22d517c273e627b5d1c54f460a8ff
lucasf-silva <fariastech19@gmail.com>
2024-11-29 11:29:23 -0300

    l

--- src/components/sidebar/OpenMenu.tsx
+++ src/components/sidebar/OpenMenu.tsx
@@ -2,17 +2,18 @@
 
 import { useMenuStore } from "@/store/menuStore";
 import { MenuOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 
 export const OpenMenu = () => {
     const { setMenu } = useMenuStore();
 
     return (
-        <button
+        <Button
             className="p-1 mr-5 -ml-1 rounded-md lg:hidden focus:outline-none focus:shadow-outline-purple"
             onClick={() => setMenu("open")}
             aria-label="Menu"
         >
             <MenuOutlined className="text-white" />
-        </button>
+        </Button>
     )
 }
